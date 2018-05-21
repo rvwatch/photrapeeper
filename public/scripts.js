b026cfb99b816783326c46b2309e5b38df3917f4
Ricardo Viera <ricardoclean@gmail.com>
2018-05-21 11:52:27 -0600

    Reorganizing function in scripts.js

--- public/scripts.js
+++ public/scripts.js
@@ -1,5 +1,37 @@
-const photoForm = $('#photrapeeper-form')[0];
+$( document ).ready( async function () {
+  const photoList = await getPhotos();
+  renderPhotoCards(photoList);
+});
 
+const getPhotos = async () => {
+  try {
+    const response = await fetch('/api/v1/albums')
+    const photos = await response.json();
+    return photos;
+    
+  } catch (err) {
+    console.log('theres nothing here...');
+  }
+}
+
+const renderPhotoCards = (photoList) => {
+  photoList.forEach(photo => {
+    const {title, url} = photo
+    
+    const cardHtml = `
+      <article class="photo-card">
+        <div class="photo-frame">
+          <img src=${url} alt=${title} />
+        </div>
+        <h2 class="photo-title">${title}</h2>
+        <button class="delete-photo">x</button>
+      </article>`;
+
+      $(".album-wrap").after(cardHtml)
+  })
+}
+
+const photoForm = $('#photrapeeper-form')[0];
 
 $(photoForm).on('submit', (event) => {
   event.preventDefault();
@@ -7,5 +39,9 @@ $(photoForm).on('submit', (event) => {
   const url = $('#url-input').val();
 
   console.log(title, url);
+
   $(photoForm)[0].reset();
 })
+
+
+
